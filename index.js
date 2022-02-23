b1ea176236fed3a574dbbedbaf52a072275e0136
liuzhipeng <liuzhipeng@haxibiao.com>
2022-02-23 21:49:15 +0800

    export module

--- index.js
+++
@@ -1,7 +0,0 @@
-// main index.js
-
-import { NativeModules } from 'react-native';
-
-const { Webserver } = NativeModules;
-
-export default Webserver;
---
+++ index.ts
@@ -0,0 +1,4 @@
+// main index.js
+
+export {default as WebServer} from './example/WebServer';
+
